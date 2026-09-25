e601879d80d76d27c17f6b5eb95f794e9b1f95e9
jwblindadopordeus-lang <jw.blindado.por.deus@gmail.com>
2026-09-24 22:23:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,5 @@
 const CACHE_NAME = "desapegai-v1";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});const CACHE_NAME = "desapegai-v1";
-
 const FILES = [
   "./",
   "./index.html",
@@ -31,6 +11,18 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES);
     })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
 });
 
